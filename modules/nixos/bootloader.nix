863288bea835f01ce149e988a6b897366dd9bb1a
EthanJ-Brady <git@ethanbrady.xyz>
2025-04-24 17:08:25 -0600

    Disabled silent boot

--- modules/nixos/bootloader.nix
+++ modules/nixos/bootloader.nix
@@ -14,23 +14,23 @@
       };
 
       # Enable "Silent Boot"
-      consoleLogLevel = 0;
-      initrd.verbose = false;
-      kernelParams = [
-        "quiet"
-        "splash"
-        "boot.shell_on_fail"
-        "loglevel=3"
-        "rd.systemd.show_status=false"
-        "rd.udev.log_level=3"
-        "udev.log_priority=3"
-        "plymouth.force-splash=1"
-        "plymouth.device=all"
-      ];
+      # consoleLogLevel = 0;
+      # initrd.verbose = false;
+      # kernelParams = [
+      #   "quiet"
+      #   "splash"
+      #   "boot.shell_on_fail"
+      #   "loglevel=3"
+      #   "rd.systemd.show_status=false"
+      #   "rd.udev.log_level=3"
+      #   "udev.log_priority=3"
+      #   "plymouth.force-splash=1"
+      #   "plymouth.device=all"
+      # ];
 
       loader = {
         efi.canTouchEfiVariables = true;
-        timeout = 0;
+        # timeout = 0;
 
         grub = {
           enable = true;
