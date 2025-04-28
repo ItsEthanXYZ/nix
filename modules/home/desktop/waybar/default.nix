360297019b8916aeca1148a89350b13ca46aadab
EthanJ-Brady <git@ethanbrady.xyz>
2025-04-27 20:51:18 -0600

    Updated the waybar styling

--- modules/home/desktop/waybar/default.nix
+++ modules/home/desktop/waybar/default.nix
@@ -9,6 +9,8 @@
   };
 
   config = lib.mkIf config.custom.desktop.waybar.enable {
+    stylix.targets.waybar.addCss = false;
+
     programs.waybar = {
       enable = true;
       settings = {
@@ -29,14 +31,14 @@
               "empty" = "";
             };
             "persistent-workspaces" = {
-              "*" = [1 2 3 4 5];
+              "*" = [1 2 3 4];
             };
           };
 
           "clock" = {
             "timezone" = "America/Denver";
             "tooltip-format" = "<tt>{calendar}</tt>";
-            "format" = "{:%Y/%m/%d %H:%M}";
+            "format" = "{:%a %Y/%m/%d %H:%M}";
             "interval" = 1;
             "calendar" = {
               "format" = {
@@ -47,7 +49,7 @@
 
           "pulseaudio#output" = {
             "scroll-step" = 5;
-            "format" = "{icon}  {volume}%";
+            "format" = "{icon} {volume}%";
             "format-muted" = "";
             "format-icons" = {
               "default" = ["" "" " "];
@@ -57,12 +59,100 @@
           "pulseaudio#input" = {
             "scroll-step" = 0;
             "format" = "{format_source}";
-            "format-source" = "";
-            "format-source-muted" = "";
+            "format-source" = "󰍬";
+            "format-source-muted" = "󰍭";
             "on-click" = "wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
           };
         };
       };
+
+      style = ''
+        window#waybar {
+          background-color: transparent;
+        }
+
+        .modules-left {
+          padding: 8px;
+          margin: 10 0 5 10;
+          border-radius: 8px;
+          background: alpha(@base00, 0.6);
+          box-shadow: 0 0 2px rgba(0, 0, 0, 0.75);
+        }
+
+        .modules-center {
+          padding: 8px;
+          margin: 10 0 5 10;
+          border-radius: 8px;
+          background: alpha(@base00, 0.6);
+          box-shadow: 0 0 2px rgba(0, 0, 0, 0.75);
+        }
+
+        .modules-right {
+          padding: 8px;
+          margin: 10 10 5 0;
+          border-radius: 8px;
+          background: alpha(@base00, 0.6);
+          box-shadow: 0 0 2px rgba(0, 0, 0, 0.75);
+        }
+
+        #clock {
+          padding: 0 4px;
+        }
+
+        #workspaces {
+          padding: 0 4px;
+        }
+        #workspaces button {
+          all:unset;
+          padding: 0px 4px;
+          color: alpha(@base0D,.4);
+          transition: all .2s ease;
+        }
+        #workspaces button:hover {
+          color:rgba(0,0,0,0);
+          border: none;
+          text-shadow: 0px 0px 1.5px rgba(0, 0, 0, .5);
+          transition: all 1s ease;
+        }
+        #workspaces button.active {
+          color: @base0D;
+          border: none;
+          text-shadow: 0px 0px 2px rgba(0, 0, 0, .5);
+        }
+        #workspaces button.empty {
+          color: rgba(0,0,0,0);
+          border: none;
+          text-shadow: 0px 0px 1.5px rgba(0, 0, 0, .2);
+        }
+        #workspaces button.empty:hover {
+          color: rgba(0,0,0,0);
+          border: none;
+          text-shadow: 0px 0px 1.5px rgba(0, 0, 0, .5);
+          transition: all 1s ease;
+        }
+        #workspaces button.empty.active {
+          color: @base0D;
+          border: none;
+          text-shadow: 0px 0px 2px rgba(0, 0, 0, .5);
+        }
+
+        #pulseaudio {
+          color: @base0B;
+        }
+        #pulseaudio.input {
+          padding: 0 4px 0 0;
+        }
+        #pulseaudio.output {
+          padding: 0 8px 0 0;
+        }
+
+        #pulseaudio.output.sink-muted {
+          color: @base08;
+        }
+        #pulseaudio.input.source-muted {
+          color: @base08;
+        }
+      '';
     };
 
     home.packages = with pkgs; [
