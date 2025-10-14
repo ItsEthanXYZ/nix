03414e5749ac5be3baf3c201046846c1b87bf8de
Ethan <git@ethanbrady.xyz>
2025-10-14 13:26:02 -0600

    Cleaned up the flake.nix to just handle newton while I handle the refactor

--- flake.nix
+++ flake.nix
@@ -6,112 +6,27 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    mandelbrust = {
-      url = "github:EthanJ-Brady/mandelbrust";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-gaming.url = "github:fufexan/nix-gaming";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    stylix.url = "github:danth/stylix";
   };
 
   outputs = {
-    nixpkgs,
-    nix-darwin,
     home-manager,
+    nix-darwin,
+    nixpkgs,
     ...
   } @ inputs: let
     systems = ["x86_64-linux" "aarch64-darwin"];
     forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
   in {
-    nixosConfigurations = {
-      "bernoulli" = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/bernoulli/configuration.nix
-          ./hosts/bernoulli/hardware-configuration.nix
-          ./modules/nixos
-          inputs.nixos-hardware.nixosModules.asus-zephyrus-ga502
-          inputs.stylix.nixosModules.stylix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
-            };
-            home-manager.users.ethan = {
-              imports = [
-                ./hosts/bernoulli/home.nix
-                ./modules/home
-              ];
-            };
-          }
-        ];
-      };
-
-      "morse" = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/morse/configuration.nix
-          ./hosts/morse/hardware-configuration.nix
-          ./modules/nixos
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
-            };
-            home-manager.users.ethan = {
-              imports = [
-                ./hosts/morse/home.nix
-                ./modules/home
-              ];
-            };
-          }
-        ];
-      };
-
-      "mohs" = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/mohs/configuration.nix
-          ./hosts/mohs/hardware-configuration.nix
-          ./modules/nixos
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
-            };
-            home-manager.users.ethan = {
-              imports = [
-                ./hosts/mohs/home.nix
-                ./modules/home
-              ];
-            };
-          }
-        ];
-      };
-    };
-
     darwinConfigurations."newton" = nix-darwin.lib.darwinSystem {
       modules = [
         ./hosts/newton/configuration.nix
         ./modules/darwin
         home-manager.darwinModules.home-manager
-        inputs.stylix.darwinModules.stylix
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
@@ -126,17 +41,6 @@
           };
         }
       ];
-    };
-
-    homeConfigurations = {
-      newton = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {system = "aarch64-darwin";};
-        extraSpecialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/newton/home.nix
-          ./modules/home
-        ];
-      };
     };
 
     devShells = forAllSystems (
