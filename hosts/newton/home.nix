8eefb80ceccaf46f3c0ecb74e008aaecaa0a3fae
Ethan <git@ethanbrady.xyz>
2025-10-14 13:32:04 -0600

    Cleaned up the home.nix file for newton

--- hosts/newton/home.nix
+++ hosts/newton/home.nix
@@ -5,19 +5,8 @@
     desktop.enable = true;
   };
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "24.05";
-
-  home.username = "ethanbrady";
-  home.homeDirectory = "/Users/ethanbrady";
-
-  # Let Home Manager install and manage itself.
+  home.username = "ethan";
+  home.homeDirectory = "/Users/ethan";
   programs.home-manager.enable = true;
 }
