4b6c1367331cfddb19f8dc1340fcfb9680ffdcd5
Ethan <git@ethanbrady.xyz>
2025-10-16 11:18:30 -0600

    Uninstalled rust packages

--- hosts/newton/home.nix
+++ hosts/newton/home.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ lib, ... }:
 {
   itsEthan = {
     programming.enable = true;
@@ -8,9 +8,4 @@
   home.username = lib.mkForce "ethan";
   home.homeDirectory = lib.mkForce "/Users/ethan";
   programs.home-manager.enable = true;
-
-  home.packages = with pkgs; [
-    rustc
-    cargo
-  ];
 }
