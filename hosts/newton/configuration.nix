f3a3d64567b1e552d95c66e7de84483337c9a363
Ethan <git@ethanbrady.xyz>
2025-10-21 21:03:45 -0600

    Added homebrew support for newton

--- hosts/newton/configuration.nix
+++ hosts/newton/configuration.nix
@@ -12,6 +12,17 @@
     watchIdAuth = true;
   };
 
+  homebrew = {
+    enable = true;
+    casks = [ "ghostty" ];
+  };
+
+  environment.extraInit = ''
+    if [ -x /opt/homebrew/bin/brew ]; then
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
+  '';
+
   system.stateVersion = 5;
   nixpkgs.hostPlatform = "aarch64-darwin";
   system.primaryUser = "ethan";
